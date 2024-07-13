8b673eaba95a311873bcb6b7f6e4207020bb566b
Juice <arenash8@gmail.com>
2024-07-12 21:22:13 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -6,11 +6,13 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    
+    # Create server socket
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    print("Server is listening on port 4221")
 
+    client, addr = server_socket.accept()  # wait for client
+    while client:
+        client.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__":    main()
+
